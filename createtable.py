4a54e53e86933c2c92d14406a3a72dcde5419dbc
RishithaReddyGaddam <44085949+RishithaReddyGaddam@users.noreply.github.com>
2019-12-12 18:12:57 +0530

    Update createtable.py

--- createtable.py
+++ createtable.py
@@ -1,5 +1,5 @@
 import mysql.connector
-conn=mysql.connector.connect(user='root',password='rishitha24',host='localhost',database='project')
+conn=mysql.connector.connect(user='root',password='***********',host='localhost',database='project')
 cursor=conn.cursor()
 cursor.execute("""create table attendance(id int,classes int)""")
 cursor.close()
